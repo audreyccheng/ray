66330c407f055b6de4fe3ebe956839b3d66fa1a2
Audrey Cheng <accheng@c02d56vvmd6r.hsd1.ca.comcast.net>
2020-09-29 15:53:51 -0700

    added resource allocation

--- benchmark/kv_store.py
+++ benchmark/kv_store.py
@@ -8,14 +8,6 @@ import ray
 import time
 import threading
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-	"--num-clients", help="number of client actors used", default=1, type=int)
-parser.add_argument(
-	"--num-servers", help="number of server actors used", default=1, type=int)
-parser.add_argument(
-	"--num-requests", help="number of requests per client", default=10000, type=int)
-
 @ray.remote
 class Server(object):
 	def __init__(self):
@@ -42,7 +34,7 @@ class Server(object):
 	# def delete(self, key):
 	# 	return self.kvstore.pop('key', None)
 
-@ray.remote
+@ray.remote(num_cpus=0)
 class Client(object):
 	def __init__(self, servers):
 		self.servers = servers
@@ -56,21 +48,92 @@ class Client(object):
 			self.servers[rand_server].get.remote(randrange(100))
 
 if __name__ == "__main__":
+	import argparse
+	parser = argparse.ArgumentParser(description="Run the key-value store.")
+	
+	parser.add_argument("--num-clients", default=1, type=int)
+	parser.add_argument("--num-servers", default=1, type=int)
+	parser.add_argument("--num-clients-per-node", default=1, type=int)
+	parser.add_argument("--num-servers-per-node", default=1, type=int)
+	parser.add_argument("--num-requests", default=10000, type=int)
+	parser.add_argument("--num-nodes", required=True, type=int)
 	args = parser.parse_args()
+	parser = argparse.ArgumentParser()
 
-	ray.init()
+	ray.init(address="auto")
+
+	nodes = [node for node in ray.nodes() if node["Alive"]]
+	while len(nodes) < args.num_nodes:
+		time.sleep(1)
+		print("{} nodes found, waiting for nodes to join".format(len(nodes)))
+		nodes = [node for node in ray.nodes() if node["Alive"]]
+
+	import socket
+	ip_addr = socket.gethostbyname(socket.gethostname())
+	node_resource = "node:{}".format(ip_addr)
+
+	for node in nodes:
+		if node_resource in node["Resources"]:
+			if "head" not in node["Resources"]:
+				ray.experimental.set_resource("head", 100, node["NodeID"])
+
+	for node in nodes:
+		for resource in node["Resources"]:
+			if resource.startswith("client") or resource.startswith("server") or resource.startswith("worker"):
+				ray.experimental.set_resource(resource, 0, node["NodeID"])
+
+	nodes = [node for node in ray.nodes() if node["Alive"]]
+	print("All nodes joined")
+	for node in nodes:
+		print("{}:{}".format(node["NodeManagerAddress"], node["NodeManagerPort"]))
+
+	head_node = [node for node in nodes if "head" in node["Resources"]]
+	assert len(head_node) == 1
+	head_ip = head_node[0]["NodeManagerAddress"]
+	nodes.remove(head_node[0])
+
+	client_resources = ["client:{}".format(i) for i in range(args.num_clients // args.num_clients_per_node)]
+	server_resources = ["server:{}".format(i) for i in range(args.num_servers // args.num_servers_per_node)]
+	owner_ip = None
+	owner_resource = None
+	node_index = 0
+
+	# for resource in server_resources:
+	# 	if "CPU" not in head_node[0]["Resources"]:
+	# 		continue
+
+	# 	print("Assigning", resource, "to node", head_node[0]["NodeID"], head_node[0]["Resources"])
+	# 	ray.experimental.set_resource(resource, 100, head_node[0]["NodeID"])
+
+	# for i in range(len(client_resources)):
+	# 	node = nodes[i % len(nodes)]
+	# 	if "CPU" not in node["Resources"]:
+	# 		continue
+
+	# 	print("Assigning", resource, "to node", node["NodeID"], node["Resources"])
+	# 	ray.experimental.set_resource(resource, 100, node["NodeID"])
+
+	assert len(nodes) >= len(client_resources) + len(server_resources)
+	for node, resource in zip(nodes, client_resources + server_resources):
+		if "CPU" not in node["Resources"]:
+			continue
+
+		print("Assigning", resource, "to node", node["NodeID"], node["Resources"])
+		ray.experimental.set_resource(resource, 100, node["NodeID"])
 
 	# server = Server.remote()
-	servers = [Server.remote() for _ in range(args.num_servers)] # options(max_concurrency=10000).
+	servers = [Server.options(resources={server_resources[i % len(server_resources)]: 1}).remote(
+		) for i in range(args.num_servers)] # options(max_concurrency=10000).
 	# client = Client.remote(servers)
-	clients = [Client.remote(servers) for _ in range(args.num_clients)]
+	clients = [Client.options(resources={client_resources[i % len(client_resources)]: 1}).remote(
+		servers) for i in range(args.num_clients)]
 
 	tstart = time.time()
 	# for _ in range(args.num_requests):
 	# 	ray.get(client.run_op.remote())
 	refs = []
 	for client in clients:
-		refs += [client.run_op.remote() for _ in range(args.num_requests)]
+		refs += [client.run_op.remote() for _ in range(args.num_requests // args.num_clients)]
 	ray.get(refs)
 
 	tend = time.time()
