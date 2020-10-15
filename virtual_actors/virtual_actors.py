05a0a19128737d7eeefc9e215e1bf4ee5c4a8f16
Samyukta Yagati <samyu@berkeley.edu>
2020-10-14 17:04:26 -0700

    Added 'generics' to virtual_actors.py

--- virtual_actors/virtual_actors.py
+++ virtual_actors/virtual_actors.py
@@ -19,13 +19,14 @@ class PhysicalActor(object):
 		# Instances of virtual actors only exist on physical actor
 		self.virtual_actors = {}
 
-	def foo(self, key):
+	def execute_task(self, method_name, key):
 		# Create virtual actor instance if key not found
 		if not key in self.virtual_actors:
 			virtual_actor = ActorClass()
 			self.virtual_actors[key] = virtual_actor
-		return self.virtual_actors[key].foo()
-
+		# Execute function on virtual actor
+		return getattr(self.virtual_actors[key],
+				method_name)()
 
 class VirtualActorGroup(object):
 	def __init__(self, actor_class):
@@ -36,16 +37,27 @@ class VirtualActorGroup(object):
 		# Allocate physical actors during construction for now, autoscaling for later
 		self.physical_actors = [PhysicalActor.remote() for _ in range(1)]
 
-	# Defaultdict API
-	# 1. Caller responsible for submitting actor task to physical actor (simpler)
-	# 2. Define Foo class on actor group: user doesn't need to deal with
-	#    physical actor handles
-	def foo(self, key):
-		if not key in self.actor_handles:
+	def __getattr__(self, method_name):
+		return PhysicalActorMethod(
+			self,
+			method_name)
+
+class PhysicalActorMethod:
+	def __init__(self, virtual_actor_group, method_name):
+		self._v_actor_gp = virtual_actor_group
+		self._method_name = method_name
+
+	def __call__(self, key, *args):
+		# Call method_name on the appropriate physical actor.
+		# System takes care of routing client request to physical actor
+		#  (vs. returning the physical actor handle to the client and
+		#  having the client execute directly on the physical actor)	
+		if not key in self._v_actor_gp.actor_handles:
 			# Many options to route virtual actors to physical ones
-			physical_actor = self.physical_actors[0]
-			self.actor_handles[key] = physical_actor
-		return self.actor_handles[key].foo.remote(key)
+			physical_actor = self._v_actor_gp.physical_actors[0]
+			self._v_actor_gp.actor_handles[key] = physical_actor
+		physical_actor = self._v_actor_gp.actor_handles[key]
+		return physical_actor.execute_task.remote(self._method_name, key)
 
 ray.init()
 virtual_actor_group = VirtualActorGroup(ActorClass)
